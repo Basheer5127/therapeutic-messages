165e7afcdd9a999e37183745d82c6f239d8b44a9
Basheer5127 <isabashir27@gmail.com>
2024-02-14 11:07:57 +0100

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Romance from "./Pages/Romance/Romance";
 const route = createBrowserRouter([
-  { path: "/Ally", element: <Romance /> },
+  { path: "/Nathy", element: <Romance /> },
   { path: "/", element: <p>Home page</p> },
 ]);
 function App() {
